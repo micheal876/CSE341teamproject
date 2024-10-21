0428a6f65e44ae814aa2efb6228316c430f96dcf
Peter <peterb@saefl.com>
2024-10-20 21:29:56 -0700

    Removed balance added orders

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,9 @@ router.use('/', require('./swagger'));
 
 //route to the page 
 router.use('/customer', require('./customer'));
-//router.use('/balance', require('./balance'));
+router.use('/order', require('./order'));
 router.use('/inventory', require('./inventory'));
-//router.use('/supplier', require('./supplier'));
+router.use('/supplier', require('./supplier'));
 router.get('/', (req, res) => {
     res.send('Home Page: Final CSE341 Project for group 9');
   });
